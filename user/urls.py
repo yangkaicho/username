2c7514c0c91753beae13a0ff592cd4505e300bff
Geogle <nike522254@gmail.com>
2022-10-17 19:46:16 +0800

    '完成初始登入功能'

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,5 @@
+from atexit import register
+import logging
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path,include
@@ -5,6 +7,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.user_register),
+    path('register/',views.user_register,name='register'),
+    path('login/',views.user_login,name='login'),
+    path('',views.profile,name='profile')
     
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from atexit import register
+from email import message
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
+def profile(request):
+    return render(request,'./user/profile.html')
+
+
+def user_login(request):
+    username=''
+    message='登入'
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+    if request.method=='POST':
+        if request.POST.get('login'):
+
+            if request.POST.get('username')=='' or request.POST.get('password')=='':
+                message='帳號或密碼不能為空'
+            else:
+                user=authenticate(request,username=username,password=password)
+                if user is None:
+                    if User.objects.filter(username=username):
+                        message='密碼有誤!'
+                    else:
+                        message='帳號有誤!'                
+                else:
+                    message='登入中...'
+                    return redirect('profile')
+
+            
+            
+            
+        if request.POST.get('register'):
+            return redirect('register')
+    return render(request,'./user/login.html',{'message':message,'username':username})
 # Create your views here.
 def user_register(request):
     
